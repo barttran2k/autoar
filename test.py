f087c22f5ef925fc0df770a419afb40e857a25c8
barttran2k <bachtran2000@gmail.com>
2023-05-05 13:53:09 +0700

    update arg Show args

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 import os,json
 from colorama import init, Fore, Style
 import keyboard
+import re
 
-# Exit khi nhấn Ctrl + C
-
+def is_valid_url(url):
+    regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https:// or ftp:// or ftps://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain
+        r'localhost|' # localhost
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return bool(regex.match(url))
     
 # Khởi tạo colorama
 init()
@@ -13,7 +21,7 @@ options = json.loads(open("options.json", "r",encoding='utf-8').read())
 
 # Hiển thị menu
 def display_menu():
-    # os.system("cls" if os.name == "nt" else "clear")  # Xóa màn hình
+    os.system("cls" if os.name == "nt" else "clear")  # Xóa màn hình
     print(Fore.GREEN + "_______________MENU___________________")
     for key, value in options.items():
         print(Fore.BLUE + key + ". " + value["name"])
@@ -22,7 +30,7 @@ def display_menu():
 
 # Hiển thị tuỳ chọn
 def display_options(selected_option):
-    # os.system("cls" if os.name == "nt" else "clear")  # Xóa màn hình
+    os.system("cls" if os.name == "nt" else "clear")  # Xóa màn hình
     if selected_option.isnumeric():
         print(Fore.GREEN + "_______________" + options[selected_option]["name"].upper() + "___________________")
         for key, value in options[selected_option]["options"].items():
@@ -33,7 +41,7 @@ def display_options(selected_option):
         
 # Hiển thị tuỳ chọn con
 def display_sub_options(selected_option,selected_sub_option):
-    # os.system("cls" if os.name == "nt" else "clear")  # Xóa màn hình
+    os.system("cls" if os.name == "nt" else "clear")  # Xóa màn hình
     print(Fore.GREEN + "_______________" + options[selected_option]["name"].upper() + "___________________")
     for key, value in options[selected_option]["options"][selected_sub_option]["options"].items():
         print(Fore.BLUE + key + ". " + value["name"])
@@ -42,18 +50,19 @@ def display_sub_options(selected_option,selected_sub_option):
 # Nhập giá trị cho tuỳ chọn
 def input_option_value(option_name):
     value = input(Fore.YELLOW + option_name + " = " + Style.RESET_ALL)
-    return value
+    return value.strip()
 
 
 # Hiển thị các giá trị đã nhập
 def display_values(values):
-    # os.system("cls" if os.name == "nt" else "clear")  # Xóa màn hình
+    os.system("cls" if os.name == "nt" else "clear")  # Xóa màn hình
     print(Fore.GREEN + "_______________VALUES___________________")
     for key, value in values.items():
         print(Fore.BLUE + value["name"] + ": " + Fore.YELLOW + value["value"])
     print(Style.RESET_ALL)
 
 def processNumber(selected_option):
+    choice = selected_option
     try:
         selected_sub_option = input(Fore.YELLOW + "Select sub-option: " + Style.RESET_ALL)
         selected_option = selected_option.upper()
@@ -78,35 +87,61 @@ def processNumber(selected_option):
                         print(Fore.RED + "Invalid sub-option. Please choose again." + Style.RESET_ALL)
                 option_name = options[selected_option]["options"][selected_sub_option]["options"][selected_sub_option_name]["name"]
                 option_arg = options[selected_option]["options"][selected_sub_option]["options"][selected_sub_option_name]["arg"]
-                if values[selected_sub_option] == "1":
-                    option_value = ""
-                else:
-                    option_value = input_option_value(option_name)
-                values[selected_sub_option] = {"name": option_name, "value": option_value, "arg":option_arg}
+                option_value = input_option_value(option_name)
+                values[selected_option] = {"name": option_name, "value": option_value, "arg":option_arg}
+                # args.append(values[selected_option])
+                display_values(values)
+                input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
+                selected_option = ""
+
+        except:
+            if choice != "1":
+                option_value = input_option_value(option_name)
+                option_arg = options[selected_option]["options"][selected_sub_option]["arg"]
+                values[selected_option] = {"name": option_name, "value": option_value, "arg":option_arg}
+                # args.append(values[selected_option])
+                display_values(values)
+                input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
+                selected_option = ""
+            else:
+                option_value = ""
+                option_arg = options[selected_option]["options"][selected_sub_option]["arg"]
+                values[selected_option] = {"name": option_name, "value": option_value, "arg":option_arg}
+                # args.append(values[selected_option])
                 display_values(values)
                 input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
                 selected_option = ""
                 
-        except:
-            if values[selected_sub_option] == "1":
-                option_value = ""
-            else:
-                option_value = input_option_value(option_name)
-            option_arg = options[selected_option]["options"][selected_sub_option]["arg"]
-            values[selected_sub_option] = {"name": option_name, "value": option_value, "arg":option_arg}
-            display_values(values)
-            input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
-            selected_option = ""
-
     except Exception as e:
         print(Fore.RED + "Invalid option. Please choose again. \n"+ e + Style.RESET_ALL)
 
 def processString(selected_option):
-    option_value = input_option_value(options[selected_option]["name"])
-    values[selected_option] = {"name": options[selected_option]["name"], "value": option_value}
-    display_values(values)
+    if selected_option != "U":
+        option_value = input_option_value(options[selected_option]["name"])
+        values[selected_option] = {"name": options[selected_option]["name"], "value": option_value}
+        # args.append(values[selected_option])
+        display_values(values)
+        input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
+        selected_option = ""
+    elif selected_option == "U":
+        option_value = input_option_value(options[selected_option]["name"])
+        while True:
+            if is_valid_url(option_value):
+                break
+            else:
+                print(Fore.RED + "Invalid URL..." + Style.RESET_ALL)
+                option_value = input_option_value(options[selected_option]["name"])
+
+        values[selected_option] = {"name": options[selected_option]["name"], "value": option_value} 
+        display_values(values)
+        input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
+        selected_option = ""
+
+def showAllValues():
+    for key, value in values.items():
+        print(Fore.BLUE + value["name"] + ": " + Fore.YELLOW + value["value"])
+    print(Style.RESET_ALL)
     input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
-    selected_option = ""
 
 def processCommand(values):
     cmd = "./archni "
@@ -119,7 +154,7 @@ def processCommand(values):
 # Chương trình chính
 selected_option = ""
 values = {}
-args = {}
+args = []
 while True:
     if keyboard.is_pressed('ctrl+c'):
         print(Fore.RED + "Exiting....." + Style.RESET_ALL)
@@ -137,7 +172,7 @@ while True:
             if selected_option.isnumeric():
                 processNumber(selected_option)
                 selected_option = ""
-            elif selected_option != "R" and not selected_option.isnumeric():
+            elif selected_option != "R" and selected_option !="S" and not selected_option.isnumeric():
                 processString(selected_option)
                 selected_option = ""
             elif selected_option == "R":
@@ -150,11 +185,14 @@ while True:
                         if key != "U":
                             cmd += value['arg'] + " " + value["value"] + " "
                     selected_option = ""
-                    print(cmd)
+                    input(cmd)
                     
                 else:
                     input(Fore.RED + "Please input URL value.\nEnter to continue" + Style.RESET_ALL)
                     selected_option = ""
+            elif selected_option == "S":
+                showAllValues()
+                selected_option = ""
     except:
         if keyboard.is_pressed('ctrl+c'):
             print(Fore.RED + "Exiting....." + Style.RESET_ALL)
