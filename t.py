6554abe0b0b1d99fd76a3a56394a8d44ffcac7da
barttran2k <bachtran2000@gmail.com>
2023-05-08 17:26:24 +0700

    update

--- t.py
+++ t.py
@@ -1,8 +1,38 @@
-import os
+import json
 
-report_dir = "report"
-dirnames = [name for name in os.listdir(report_dir) if os.path.isdir(os.path.join(report_dir, name))]
-
-print("List of directories in report folder:")
-for name in dirnames:
-    print(name)
+a = {
+   "3":{
+      "3":{
+         "arg":"--scope-exclude-pattern",
+         "value":"3"
+      },
+      "1":{
+         "arg":"--scope-include-pattern",
+         "value":"123"
+      },
+      "2":{
+         "arg":"--scope-include-subdomains",
+         "value":"123"
+      }
+   },
+   '4':{
+        "3":{
+            "arg":"--scope-exclude-pattern",
+            "value":"3"
+        },
+        "1":{
+            "arg":"--scope-include-pattern",
+            "value":"123"
+        },
+        "2":{
+            "arg":"--scope-include-subdomains",
+            "value":"123"
+        }
+   }
+}
+a = dict(a)
+print("_______________VALUES___________________")
+for key, suboptions in a.items():
+    print("" + key + ":")
+    for suboption, subvalue in suboptions.items():
+        print( "  " + subvalue['arg'] + ": "  + subvalue['value'])
--- test.py
+++ test.py
@@ -39,8 +39,6 @@ init()
 options = json.loads(open("options.json", "r", encoding='utf-8').read())
 
 # Hiển thị menu
-
-
 def display_menu():
     os.system("cls" if os.name == "nt" else "clear")  # Xóa màn hình
     print(Fore.GREEN + "_______________MENU___________________\n")
@@ -63,8 +61,6 @@ def display_options(selected_option):
               options[selected_option]["name"].upper() + "___________________")
 
 # Hiển thị tuỳ chọn con
-
-
 def display_sub_options(selected_option, selected_sub_option):
     os.system("cls" if os.name == "nt" else "clear")  # Xóa màn hình
     print(Fore.GREEN + "_______________" +
@@ -73,9 +69,8 @@ def display_sub_options(selected_option, selected_sub_option):
         print(Fore.BLUE + key + ". " + value["name"])
     print(Style.RESET_ALL)
 
+
 # Nhập giá trị cho tuỳ chọn
-
-
 def input_option_value(option_name):
     value = input(Fore.YELLOW + option_name + " = " + Style.RESET_ALL)
     return value.strip()
@@ -86,13 +81,51 @@ def display_values(values):
     os.system("cls" if os.name == "nt" else "clear")  # Xóa màn hình
     print(Fore.GREEN + "_______________VALUES___________________")
     for key, value in values.items():
-        print(Fore.BLUE + value["name"] + ": " + Fore.YELLOW + value["value"])
+        if "name" in value and "arg" not in value:  # Nếu dữ liệu theo dạng cu
+            print(Fore.BLUE + value["name"] + ": " + Fore.YELLOW + value["value"])
+        elif "name" in value and "arg" in value:  # Nếu dữ liệu theo dạng con
+            print(Fore.BLUE + value["arg"] + ": " + Fore.YELLOW + value["value"])
+        else:
+            print(Fore.BLUE + key + ":")
+            for suboption, subvalue in value.items():
+                print( Fore.GREEN + subvalue['arg'] + ": "  + subvalue['value'])
     print(Style.RESET_ALL)
+    input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
 
+# Add value
+def add_value(selected_option, sub_option, option_value, option_arg=None):
+    # Kiểm tra xem selected_option đã có trong values chưa
+    if selected_option not in values:
+        values[selected_option] = {}
+    # Kiểm tra xem sub_option đã có trong values của selected_option chưa
+    if sub_option not in values[selected_option]:
+        values[selected_option][sub_option] = {"arg": option_arg, "value": option_value}
+    else:
+        # Nếu sub_option đã tồn tại, cập nhật lại giá trị value và arg của nó
+        values[selected_option][sub_option]["arg"] = option_arg
+        values[selected_option][sub_option]["value"] = option_value
 
+# Remove value
+def remove_value(selected_option, sub_option):
+    # Kiểm tra xem selected_option và sub_option có tồn tại trong values không
+    if selected_option in values and sub_option in values[selected_option]:
+        # Xoá sub_option khỏi values của selected_option
+        del values[selected_option][sub_option]
+        # Nếu values của selected_option trống, xoá selected_option khỏi values
+        if not bool(values[selected_option]):
+            del values[selected_option]
+
+# Update value
+def update_value(selected_option, sub_option, option_name, option_value, option_arg=None):
+    add_value(selected_option, sub_option, option_name, option_value, option_arg)
+
+# Clear values
+def clear_values():
+    values.clear()
+
+# Process number choice
 def processNumber(selected_option):
     choice = selected_option
-    
     try:
         selected_sub_option = input(
             Fore.YELLOW + "Select sub-option: " + Style.RESET_ALL)
@@ -123,20 +156,19 @@ def processNumber(selected_option):
                 option_name = options[selected_option]["options"][selected_sub_option]["options"][selected_sub_option_name]["name"]
                 option_arg = options[selected_option]["options"][selected_sub_option]["options"][selected_sub_option_name]["arg"]
                 option_value = input_option_value(option_name)
-                values[(selected_option+option_name)]
-                values[selected_option] = {'name': option_name, 'value': valarg, 'arg': option_arg}
+                add_value(selected_option, selected_sub_option_name, option_value, option_arg)
                 display_values(values)
-                input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
+                
                 selected_option = ""
 
         except:
             if choice != "1" and choice != '8':
                 option_value = input_option_value(option_name)
                 option_arg = options[selected_option]["options"][selected_sub_option]["arg"]
-                values[selected_option] = {
-                    "name": option_name, "value": option_value, "arg": option_arg}
+                # Kiểm tra xem selected_option đã có trong values chưa
+                add_value(selected_option, selected_sub_option, option_value, option_arg)
                 display_values(values)
-                input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
+
                 selected_option = ""
             elif choice == "8":
                 if selected_sub_option == "1":
@@ -148,31 +180,27 @@ def processNumber(selected_option):
                     print(valarg)
                     values[selected_option] = {'name': option_name, 'value': valarg, 'arg': option_arg}
                 display_values(values)
-                input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
+                
                 selected_option = ""    
             else:
                 option_value = ""
                 option_arg = options[selected_option]["options"][selected_sub_option]["arg"]
-                values[selected_option] = {
-                    "name": option_name, "value": option_value, "arg": option_arg}
-                
+                add_value(selected_option, selected_sub_option, option_value, option_arg)
                 display_values(values)
-                input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
+                
                 selected_option = ""
 
     except Exception as e:
         print(Fore.RED + "Invalid option. Please choose again. \n" +
               e + Style.RESET_ALL)
 
-
+# Process string choice
 def processString(selected_option):
     if selected_option != "U":
         option_value = input_option_value(options[selected_option]["name"])
         values[selected_option] = {
             "name": options[selected_option]["name"], "value": option_value}
-        
         display_values(values)
-        input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
         selected_option = ""
     elif selected_option == "U":
         option_value = input_option_value(options[selected_option]["name"])
@@ -186,9 +214,9 @@ def processString(selected_option):
         values[selected_option] = {
             "name": options[selected_option]["name"], "value": option_value}
         display_values(values)
-        input(Fore.YELLOW + "Press Enter to continue..." + Style.RESET_ALL)
         selected_option = ""
 
+# Update filenames from report folder
 def updateFilenames():
     global filenames
     report_dir = "report"
@@ -199,6 +227,7 @@ def updateFilenames():
             filenames.append(i)
     filenames = list(set(filenames))
     
+# Show filenames
 def showFilenames():
     count = 1
     for i in filenames:
@@ -213,6 +242,7 @@ def showFilenames():
             break
     return int(choice)-1
 
+# Auto gen autologin plugin
 def autoPlugin():
     print(Fore.GREEN + "Auto plugin" + Style.RESET_ALL)
     print(Fore.YELLOW + "Please fill data" + Style.RESET_ALL)
@@ -229,11 +259,22 @@ def autoPlugin():
     valarg = 'autologin:url='+url+',parameters='+parameter+',check='+check
     return valarg
 
+# Show all values
 def showAllValues():
-    for key, value in values.items():
-        print(Fore.BLUE + value["name"] + ": " + Fore.YELLOW + value["value"])
-    print(Style.RESET_ALL)
-    input(Fore.YELLOW + "Press Enter to continue\n"+Style.RESET_ALL)
+    if len(values) == 0 :
+        input(Fore.RED + "No values to show. Press Enter to continue..." + Style.RESET_ALL)
+    else:
+        for key, value in values.items():
+            if "name" in value and 'arg' not in value:  # Nếu dữ liệu theo dạng cu
+                print(Fore.BLUE + value["name"] + ": " + Fore.YELLOW + value["value"])
+            if "name" in value and 'arg' in value:  # Nếu dữ liệu theo dạng cu
+                print(Fore.BLUE + value["arg"] + ": " + Fore.YELLOW + value["value"])
+            elif key.isnumeric():
+                print(Fore.BLUE + key + ":")
+                for suboption, subvalue in value.items():
+                    print( Fore.GREEN + subvalue['arg'] + ": "  + subvalue['value'])
+            print(Style.RESET_ALL)
+        input(Fore.RED + "Press Enter to continue..." + Style.RESET_ALL)
 
 def processCommand(values):
     cmd = "./archni "
@@ -243,6 +284,17 @@ def processCommand(values):
         cmd += key + "=" + value["value"] + " "
     os.system(cmd)
 
+def getArgVal():
+    argval = ""
+    for key, value in values.items():
+        if key != "U" and key != "Report":
+            if "name" in value:  # Nếu dữ liệu theo dạng cu
+                argval += value["arg"] + " " + value["value"] + " "
+            else:
+                print(Fore.BLUE + key + ":")
+                for suboption, subvalue in value.items():
+                    argval += subvalue['arg'] + " " + subvalue['value'] + " "
+    return argval
 
 # Chương trình chính
 while True:
@@ -283,11 +335,7 @@ while True:
                         filenames.append(filename)
                         cmd = "./arachni/bin/arachni " + \
                             values["U"]["value"] + " " + \
-                            " --report-save-path ./report/" + filename + ".afr "
-                        for key, value in values.items():
-                            if key != "U" and key != "Report":
-                                cmd += value['arg'] + " " + \
-                                    value["value"] + " "
+                            " --report-save-path ./report/" + filename + ".afr " + getArgVal()
                         selected_option = ""
                         print(cmd+"\n Want to continue? (Y/N)")
                         choice = input().upper()
@@ -321,7 +369,6 @@ while True:
                     selected_option = ""
             elif selected_option == "S":
                 showAllValues()
-                
                 selected_option = ""
 
             elif selected_option == "V":
