4359f1d36b6f83477fda77a2aaba7d0f7480bed8
barttran2k <bachtran2000@gmail.com>
2023-05-09 08:40:51 +0700

    update script xss

---
+++ autoXSS.py
@@ -0,0 +1,29 @@
+import subprocess,re,os
+from datetime import datetime
+
+# Nhập url cần scan
+url = input("Nhập URL: ")
+filename =  re.search("(?<=://)([a-zA-Z0-9\-\.]+)", url).group(1) + datetime.now().strftime("%d%m%H%M%S")
+
+# Chạy lệnh arachni để kiểm tra XSS trên trang web
+command = "./arachni/bin/arachni " + url + " --checks=xss* --report-save-path ./report/"+ filename + ".afr "
+process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+while process.poll() is None:
+    output = process.stdout.readline().decode().rstrip()
+    print(output)
+process.communicate()
+idpath = "report/"+filename+"/index.html"
+if os.path.exists(idpath):
+    cmd_genHTML = './arachni/bin/arachni_reporter ./report/' + filename + '.afr --reporter=html:outfile=./report/' + filename + \
+                                    '.html.zip && unzip ./report/' + filename + '.html.zip -d ./report/' + \
+                                    filename+' && rm ./report/' + filename + '.html.zip'
+    process = subprocess.Popen(
+        cmd_genHTML, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    while process.poll() is None:
+        output = process.stdout.readline().decode().rstrip()
+        print(output)
+    process.communicate()
+    input("DONE!!!\nPlease Enter to continue")
+else:
+    print("Lỗi trong quá trình kiểm tra")
+    input("DONE!!!\nPlease Enter to continue")
