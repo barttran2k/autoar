dfd7061496a0a0b661d94eb1ff0695e50fe7a8e0
barttran2k <bachtran2000@gmail.com>
2023-05-09 09:37:10 +0700

    update autoxss to load plugin autolog

--- autoXSS.py
+++ autoXSS.py
@@ -1,32 +1,90 @@
 #!/usr/bin/python3
-import subprocess,re,os
+import subprocess, re, os
 from datetime import datetime
+from colorama import init, Fore, Style
 import webbrowser
+
+init()
+
+
+def is_valid_url(url):
+    regex = re.compile(
+        r"^(?:http|ftp)s?://"  # http:// or https:// or ftp:// or ftps://
+        # domain
+        r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"
+        r"localhost|"  # localhost
+        r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # or ip
+        r"(?::\d+)?"  # optional port
+        r"(?:/?|[/?]\S+)$",
+        re.IGNORECASE,
+    )
+    return bool(regex.match(url))
+
 
 # Nhập url cần scan
 url = input("Nhập URL: ")
-filename =  re.search("(?<=://)([a-zA-Z0-9\-\.]+)", url).group(1) + datetime.now().strftime("%d%m%H%M%S")
+while True:
+    if is_valid_url(url):
+        break
+    else:
+        print(Fore.RED + "Invalid URL..." + Style.RESET_ALL)
+        url = input(Fore.YELLOW + "Input URL: " + Style.RESET_ALL)
+filename = re.search("(?<=://)([a-zA-Z0-9\-\.]+)", url).group(
+    1
+) + datetime.now().strftime("%d%m%H%M%S")
 
+print("Auto login: (y/n)")
+autoLogin = ""
+if input() == "y":
+    url = input(Fore.YELLOW + "Input URL: " + Style.RESET_ALL)
+    while True:
+        if is_valid_url(url):
+            break
+        else:
+            print(Fore.RED + "Invalid URL..." + Style.RESET_ALL)
+            url = input(Fore.YELLOW + "Input URL: " + Style.RESET_ALL)
+    parameter = input(Fore.YELLOW + "Input parameter: " + Style.RESET_ALL)
+    check = input(Fore.YELLOW + "Input check: " + Style.RESET_ALL)
+    autoLogin = "autologin:url=" + url + ",parameters=" + parameter + ",check=" + check
 # Chạy lệnh arachni để kiểm tra XSS trên trang web
-command = "./arachni/bin/arachni " + url + " --checks=xss* --report-save-path ./report/"+ filename + ".afr "
-process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+command = (
+    "./arachni/bin/arachni "
+    + url
+    + " --checks=xss* --report-save-path ./report/"
+    + filename
+    + ".afr " + autoLogin
+)
+process = subprocess.Popen(
+    command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+)
 while process.poll() is None:
     output = process.stdout.readline().decode().rstrip()
     print(output)
 process.communicate()
-cmd_genHTML = './arachni/bin/arachni_reporter ./report/' + filename + '.afr --reporter=html:outfile=./report/' + filename + \
-                                '.html.zip && unzip ./report/' + filename + '.html.zip -d ./report/' + \
-                                filename+' && rm ./report/' + filename + '.html.zip'
+cmd_genHTML = (
+    "./arachni/bin/arachni_reporter ./report/"
+    + filename
+    + ".afr --reporter=html:outfile=./report/"
+    + filename
+    + ".html.zip && unzip ./report/"
+    + filename
+    + ".html.zip -d ./report/"
+    + filename
+    + " && rm ./report/"
+    + filename
+    + ".html.zip"
+)
 process = subprocess.Popen(
-    cmd_genHTML, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    cmd_genHTML, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+)
 while process.poll() is None:
     output = process.stdout.readline().decode().rstrip()
     print(output)
 process.communicate()
 
-idpath = "report/"+filename+"/index.html"
+idpath = "report/" + filename + "/index.html"
 if os.path.exists(idpath):
-    webbrowser.open(idpath,new=2)
+    webbrowser.open(idpath, new=2)
     input("DONE!!!\nPlease Enter to continue")
 else:
     print("Lỗi trong quá trình kiểm tra")
